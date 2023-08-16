7cf7e8199150a4695e70d5d698ad27a080e394d2
stoyan kirkov <stu.kirkov@gmail.com>
2023-08-16 17:21:55 +0300

    Initial commit

---
+++ src/main/java/com/example/primenumberconsumer/PrimeNumberConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.primenumberconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberConsumerApplication.class, args);
+	}
+
+}
